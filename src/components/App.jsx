1ca54acce0fa6a5cb3d0481acf47a713dac8acff
yan_cy <yan_cy@Ctrip.com>
2020-02-27 20:04:30 +0800

    chore: 区分客户端和服务端state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom';
 
 export default class extends Component {
 
+    constructor(props) {
+        super(props);
+        if (typeof window === 'undefined') {
+            // server
+            this.state = {
+                index: 0
+            };
+            console.log('here is server side constructor');
+        } else {
+            // client
+            console.log('here is client side constructor');
+            console.log('state index: ', this.state);
+        }
+    }
+
     handleClick = () => {
         console.log('lalalalala!!!!');
+    }
+
+    componentDidMount() {
+        console.log('did mount');
     }
 
     render() {
