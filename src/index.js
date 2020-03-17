cc902ba2c4c3b6bbb71a8af6af1b18b2acba89e5
yan_cy <yan_cy@Ctrip.com>
2020-03-17 21:21:59 +0800

    chore: remove service worker

--- src/index.js
+++ src/index.js
@@ -12,3 +12,13 @@ loadableReady(() => {
         document.getElementById('root')
     );
 });
+
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('/sw.js').then(reg => {
+//       // registration worked
+//       console.log('Registration succeeded. Scope is ' + reg.scope);
+//     }).catch(error => {
+//       // registration failed
+//       console.log('Registration failed with ' + error);
+//     });
+//   }
